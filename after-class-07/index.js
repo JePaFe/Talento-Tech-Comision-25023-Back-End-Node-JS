e12ad64e09685d9d0c9080dbb55063919c1a889a
Jean Paul Ferreira <jeanpaul@jepafe.net>
2025-05-13 21:18:46 -0300

    After Class 07 - NPM init y Modules

---
+++ after-class-07/index.js
@@ -0,0 +1,19 @@
+// import { argv } from "node:process";
+
+// console.log("After Class");
+
+// if (argv[2].toLowerCase() == "GET".toLowerCase()) {
+//   console.log("El parametro es GET");
+// }
+
+// console.log(argv.includes("GET"));
+
+// if (argv[2].toLowerCase() == "POST".toLowerCase()) {
+//   console.log("El parametro es POST y el ID es: " + argv[3]);
+// }
+
+// console.log(argv.indexOf("GET"));
+
+import * as math from "./math.js";
+
+console.log(math.restar);
---
+++ after-class-07/math.js
@@ -0,0 +1,19 @@
+export const datos = [];
+
+// export default function sumar(a, b) {
+//   return a + b;
+// }
+
+// export default (a, b) => {
+//   return a + b;
+// };
+
+export const restar = (a, b) => a - b;
+
+// export function restar(a, b) {
+//   return a - b;
+// }
+
+// export default {
+//     restar
+// }
